92aedcf901f619ae208194eed2f32158fe55d3c5
Matt Blair <mdrblair@gmail.com>
2021-01-12 16:45:01 +0000

    - Fixing broken path in 'includes > root-level > head.php' - Removing IE8 support through HTML5shiv and respond.js (no longer a supported browser)

--- includes/head.php
+++ includes/head.php
@@ -32,11 +32,6 @@
           href="../images/utilities/favicon.png">
     <link rel="icon" type="image/vnd.microsoft.icon"
           href="../images/utilities/favicon.png">
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!--[if lt IE 9]>
-    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
 
     <!-- Google Tag Manager -->
     <script>
